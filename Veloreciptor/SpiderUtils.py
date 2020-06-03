253f9e2d308fefd7d3f3d3de002d71b320d59c9f
MatiFilozof <matibakala@gmail.com>
2020-06-03 14:33:08 +0200

    removing trailing whitespaces

--- Veloreciptor/SpiderUtils.py
+++ Veloreciptor/SpiderUtils.py
@@ -2,4 +2,4 @@ import re
 
 
 def add_space_before_unit(ingredient):
-    return re.sub(r'([0-9/\-,]+)([A-Za-zĄąĆćĘęŃńŚśŻżŹźÓóŁł]+)', r'\1 \2', ingredient)
+    return re.sub(r'([0-9/\-,]+)([A-Za-zĄąĆćĘęŃńŚśŻżŹźÓóŁł]+)', r'\1 \2', ingredient).strip()
